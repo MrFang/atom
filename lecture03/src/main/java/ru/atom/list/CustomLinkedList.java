557e20c8d30aecdaca53da1080c1ab600a26b54d
Zolotukhin Fedor <bat312@yandex.ru>
2019-07-28 18:42:24 +0300

    Done lecture03 practice

--- lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
+++ lecture03/src/main/java/ru/atom/list/CustomLinkedList.java
@@ -7,55 +7,129 @@ import java.util.ListIterator;
 
 
 public class CustomLinkedList<E> implements List<E> {
+    final ListNode<E> entry = new ListNode<E>();
+    int size;
+
+    public CustomLinkedList() {
+        entry.next = entry.prev = entry;
+        size = 0;
+    }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException();
+
+        for (E item : this) {
+            if (item.equals(o)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<E>() {
+            ListNode<E> current = entry.next;
+
+            @Override
+            public boolean hasNext() {
+                return current != entry;
+            }
+
+            @Override
+            public E next() {
+                E value = current.data;
+                current = current.next;
+                return value;
+            }
+        };
     }
 
     @Override
     public boolean add(E e) {
-        throw new UnsupportedOperationException();
+        ListNode<E> newNode = new ListNode<E>(e);
+
+        newNode.prev = entry.prev;
+        newNode.next = entry;
+        entry.prev.next = newNode;
+        entry.prev = newNode;
+
+        size++;
+
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
+        ListNode<E> current = entry.next;
+
+        while (current != entry) {
+
+            if (current.data.equals(o)) {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+
+                size--;
+            }
+
+            current = current.next;
+        }
+
+        return true;
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        entry.prev = entry.next = null;
+        size = 0;
     }
 
     @Override
     public E get(int index) {
-        throw new UnsupportedOperationException();
+        int ind;
+        ListNode<E> current = entry.next;
+
+        for (ind = 0; ind < index; ++ind) {
+            current = current.next;
+        }
+
+        return current.data;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException();
+        ListNode<E> current = entry.next;
+
+        for (int i = 0; i < size; ++i) {
+
+            if (current.data.equals(o)) {
+                return i;
+            }
+
+            current = current.next;
+        }
+
+        return -1;
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
-        throw new UnsupportedOperationException();
+        for (E item : c) {
+            add(item);
+        }
+
+        return true;
     }
 
 
--- lecture03/src/main/java/ru/atom/list/ListNode.java
+++ lecture03/src/main/java/ru/atom/list/ListNode.java
@@ -4,4 +4,17 @@ package ru.atom.list;
  * Contains ref to next node, prev node and value
  */
 public class ListNode<E> {
+    public E data;
+    ListNode<E> next;
+    ListNode<E> prev;
+
+    public ListNode() {
+        this.data = null;
+        this.next = this.prev = null;
+    }
+
+    public ListNode(E data) {
+        this.data = data;
+        this.next = this.prev = null;
+    }
 }
